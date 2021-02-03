b7207cbf61d6e37fda5d48ebaa2cc9e1b003ca03
Anush Pallaev <77150208+Nushko@users.noreply.github.com>
2021-02-03 11:27:03 -0300

    Add files via upload

---
+++ lesson7_02.02.21/lesson7_02.02.21/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lesson7_02._02._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            lesson7_table person1 = new lesson7_table
+            {
+                Name = "Ануш",
+                PhoneNumber = "8-800-555-35-35",
+                Address = "Дом",
+            };
+
+            lesson7_table person2 = new lesson7_table
+            {
+                Name = "Агуш",
+                Address = "Дом Агушы",
+                Job = "Бариста"
+            };
+
+        }
+    }
+
+    class lesson7_table
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Job { get; set; }
+    }
+}
